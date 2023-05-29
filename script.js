537579f47d1b936328cd53b04475b9abd3c641e0
Lourdes <121977047+LourdesN@users.noreply.github.com>
2023-05-29 08:58:50 +0000

    activate searchbar

--- script.js
+++ script.js
@@ -43,4 +43,7 @@ for (let item of anchorlinks)
   })
 }
 
-
+document.getElementById('frmSearch').onsubmit = function() {
+  window.location = 'http://www.google.com/search?q=site:yoursitename.com ' + document.getElementById('txtSearch').value;
+  return false;
+}
