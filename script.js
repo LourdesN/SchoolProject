10cbdd049dfb204f91241025cc0d958c9d29cca1
Lourdes <121977047+LourdesN@users.noreply.github.com>
2023-05-29 10:49:40 +0000

    form validation

--- script.js
+++ script.js
@@ -47,3 +47,74 @@ document.getElementById('frmSearch').onsubmit = function() {
   window.location = 'http://www.google.com/search?q=site:https://lourdesn.github.io/SchoolProject/ ' + document.getElementById('txtSearch').value;
   return false;
 }
+
+function validateName() {
+
+  var name = document.getElementById('fname').value;
+
+  if(name.length == 0) {
+
+    producePrompt('Name is required', 'name-error' , 'red')
+    return false;
+
+}
+
+if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+
+    producePrompt('First and last name, please.','name-error', 'red');
+    return false;
+
+}
+
+producePrompt('Valid', 'name-error', 'green');
+return true;
+
+}
+function validateEmail () {
+
+  var email = document.getElementById('email').value;
+  
+  if(email.length == 0) {
+  
+    producePrompt('Email Invalid','email-error', 'red');
+    return false;
+  
+  }
+  
+  if(!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+  
+    producePrompt('Email Invalid', 'email-error', 'red');
+    return false;
+  }
+  
+  producePrompt('Valid', 'email-error', 'green');
+  return true;
+  
+  }
+  function validateForm() {
+    if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage()) {
+      jsShow('submit-error');
+      producePrompt('Please fix errors to submit.', 'submit-error', 'red');
+      setTimeout(function(){jsHide('submit-error');}, 2000);
+      return false;
+    }
+    else {
+    
+    }
+    }
+    
+    function jsShow(id) {
+    document.getElementById(id).style.display = 'block';
+    }
+    
+    function jsHide(id) {
+    document.getElementById(id).style.display = 'none';
+    }
+    
+    
+    function producePrompt(message, promptLocation, color) {
+    
+    document.getElementById(promptLocation).innerHTML = message;
+    document.getElementById(promptLocation).style.color = color;
+    
+    }
